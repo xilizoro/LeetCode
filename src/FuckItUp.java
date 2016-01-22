7ea696fe2c94dd38d0c406ee62010f2ffac1f9d1
xilizoro <xianggao@xiangdeMBP.home>
2016-01-22 14:08:09 -0500

    First Try for Git

---
+++ src/FuckItUp.java
@@ -0,0 +1,10 @@
+/**
+ * Created by xianggao on 16/1/16.
+ */
+public class FuckItUp {
+    public static void main(String[]args){
+        System.out.println("I'm gonna make it!!!");
+        System.out.println("can you believe it?");
+        System.out.println("Do you believe it?");
+    }
+}
